61fbd4845e17c6473a230297e8336f4c1388b060
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2017-11-09 22:39:04 -0500

    Added comments

--- tools/wiki_scrp.js
+++ tools/wiki_scrp.js
@@ -1,16 +1,22 @@
+//required node modules
 var fs = require('fs');
 var scrapy = require('node-scrapy');
 var request = require('request');
 
+
+//read in the data in our list file
 var readableStream = fs.createReadStream('./lists/gold_mine.txt');
-var desc = "gold mine";
-
-var data = '';
+//set the encoding of the data stream
 readableStream.setEncoding('utf8');
+//establish the data variable for the incoming data
+var data = '';
 
+//this variable is used to set the description field
+var desc = "gold mine";
+//this is a date contant to set the added  field
 const now = new Date();
-console.log(now);
 
+//the data model for node-scrapy
 var model = {
     'name': '.firstHeading',
     'geo': {
@@ -19,10 +25,15 @@ var model = {
     },
 };
 
+//read in the data
 readableStream.on('data', function(chunk) {
     data += chunk;
 });
 
+//split the data on commas into an array
+//trim out extra characters and spaces
+//cycle through the array
+//call the gather function to scrape data and create JSON
 readableStream.on('end', function() {
     var list = data.split(',');
 
@@ -38,10 +49,11 @@ readableStream.on('end', function() {
 })
 
 function gather(place, nm) {
-
+  //use wikipedia api to search for data
   var wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=' +
       '&search=' + place;
 
+    //make the request to the api
     request(wikiapi, function(err, res, body) {
         if (err != null) {
             console.log("Request " + err);
@@ -50,24 +62,22 @@ function gather(place, nm) {
             if (!parsed[3]) {
                 return
             }
+            //find the article url in the returned api data
             var url = parsed[3][0];
-            //console.log(parsed)
-            //fs.appendFile('log.txt', parsed);
-            //console.log(url);
 
-
+            //pass the url into scrapy
             scrapy.scrape(url, model, function(err, data) {
                 if (err) {
                     console.log(nm);
                     console.log(wikiapi);
+                    //record searches that don't work
                     fs.appendFile('broken.txt', nm);
-                    //console.error("Scrape " + err + " " + url);
                     return
                 }
-
+                //convert js values to JSON
                 var json = JSON.stringify(data, null, 2);
+                //parse JSON to objects
                 var parsed = JSON.parse(json);
-
 
                 if (parsed.geo != null) {
                     var latlon = parsed.geo.split(';');
@@ -87,18 +97,15 @@ function gather(place, nm) {
                     return
                 }
 
-
-
+                //convert all values to JSON
                 var rejson = JSON.stringify(parsed, null, 2);
-                //console.log(rejson);
 
+                //add this entry to JSON file
                 fs.appendFile('./json/gold_mine.json', '\n\n' + rejson + ',', finished);
 
                 function finished(err) {
-
                 }
             })
-
         }
     })
 }
