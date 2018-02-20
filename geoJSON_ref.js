ee6c9ca032573382ebf378c22b55c338b704f4e0
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-02-20 16:35:13 -0500

    Data structure notes/ref

---
+++ geoJSON_ref.js
@@ -0,0 +1,43 @@
+"properties":
+{
+  //state name
+  "Name": "Alabama",
+  //???
+  "description": "<center><table><tr><th colspan='2' align='center'><em>Attributes<\/em><\/th><\/tr><tr bgcolor=\"#E3E3F3\">\n<th>STATEFP<\/th>\n<td>01<\/td>\n<\/tr><tr bgcolor=\"\">\n<th>STATENS<\/th>\n<td>01779775<\/td>\n<\/tr><tr bgcolor=\"#E3E3F3\">\n<th>AFFGEOID<\/th>\n<td>0400000US01<\/td>\n<\/tr><tr bgcolor=\"\">\n<th>GEOID<\/th>\n<td>01<\/td>\n<\/tr><tr bgcolor=\"#E3E3F3\">\n<th>STUSPS<\/th>\n<td>AL<\/td>\n<\/tr><tr bgcolor=\"\">\n<th>NAME<\/th>\n<td>Alabama<\/td>\n<\/tr><tr bgcolor=\"#E3E3F3\">\n<th>LSAD<\/th>\n<td>00<\/td>\n<\/tr><tr bgcolor=\"\">\n<th>ALAND<\/th>\n<td>131173688951<\/td>\n<\/tr><tr bgcolor=\"#E3E3F3\">\n<th>AWATER<\/th>\n<td>4593686489<\/td>\n<\/tr><\/table><\/center>",
+  //???
+  "timestamp": null,
+  //???
+  "begin": null,
+  //???
+  "end": null,
+  //???
+  "altitudeMode": "clampToGround",
+  //???
+  "tessellate": 1,
+  //???
+  "extrude": 0,
+  //???
+  "visibility": 1,
+  //???
+  "drawOrder": null,
+  //???
+  "icon": null,
+  //Current state Federal Information Processing Series (FIPS) code
+  "STATEFP": "01",
+  //GNIS Lookup number
+  "STATENS": "01779775",
+  //American FactFinder ID (Census Database)
+  "AFFGEOID": "0400000US01",
+  //State identifier; state FIPS code (Federal Information Processing Standard) (Depreciated)
+  "GEOID": "01",
+  //Current United States Postal Service state abbreviation
+  "STUSPS": "AL",
+  //????
+  "NAME2": null,
+  //Current legal/statistical area description code for state
+  "LSAD": "00",
+  //Current land area (square meters)
+  "ALAND": "131173688951",
+  //Current water area (square meters)
+  "AWATER": "4593686489"
+  }
