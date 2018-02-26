39f129b96f0e192e4f6369bf5037785c8567b576
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-02-26 17:32:23 -0500

    d3 updates

--- public/script.js
+++ public/script.js
@@ -1,8 +1,33 @@
-let usGeoJSON = '~/tools/cb_2016_us_state_500k_edit_v2.geojson'
+let usGeoJSON = 'cb_2016_us_state_500k_edit_v2.geojson';
+
+var projection = d3.geo.albers();
+var path = d3.geo.path().projection(projection);
 
 
-d3.json("http://localhost:3000/api/places", function(data) {
-  console.log(data);
+d3.json(usGeoJSON, function(data){
 
-  var map = d3.select('box').
-});
+  var mapContainer = d3.select('#mapContainer').append('svg')
+      .attr("preserveAspectRatio", "xMinYMin meet")
+      .attr("viewBox", "0 0 960 500")
+      .classed("card", true)
+
+  var group = mapContainer.selectAll('g')
+     .data(data.features)
+     .enter()
+     .append('g')
+
+ var map = group.append("path")
+     .attr("d", path)
+     .attr("class", "area")
+     .style({
+       fill: 'none',
+       stroke: "#6E6E6E",
+     })
+})
+
+
+// d3.json("http://localhost:3000/api/places", function(data) {
+//   console.log(data);
+//
+//   var map = d3.select('').
+// })
