a1ca0d82e3300d1d91df58206905022a0bf31a5d
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-07 16:06:28 -0500

    Updating maps

--- public/leaf.js
+++ public/leaf.js
@@ -1,8 +1,42 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var url = "http://localhost:3000/api/places";
+var myMap = L.map('mapid').setView([39.648734, -119.9761848], 2.5);
+var myKey = config.MY_KEY;
+var placeMarkers = [];
+
+var myIcon = L.icon({
+    iconUrl: 'placeMarker.png'
+  })
+//I do not understand this promise
+fetch(url)
+  .then(res => res.json())
+  .then((data) => {
+    console.log('Checkout this JSON! ', data);
+    for (var i = 0; i < data.length; i++) {
+      placeMarker[i] = L.marker([data[i].location.coordinates[1], data[i].location.coordinates[0]], {icon: myIcon})
+      placeMarker[i].addTo(myMap)
+    }
+  })
+
+  .catch(err => { throw err });
+
+  console.log('wow!');
+
+
+
 
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
-    accessToken: 'your.mapbox.access.token'
-}).addTo(mymap);
+    accessToken: myKey
+}).addTo(myMap);
+
+
+function placeMarker(lat, lon){
+  var lat = this.lat;
+  var lon = this.lon;
+
+  this.show = function(){
+    L.marker([lat, lon]).addTo(myMap);
+  }
+}
