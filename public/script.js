d8891353ef6f770836d7af870ddad2a78ffcbf68
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-14 12:08:11 -0400

    Creates Boxes

--- public/script.js
+++ public/script.js
@@ -12,5 +12,46 @@ function loadJSON(url){
 
 function drawBoxes(data){
   console.log(data);
-  
+
+  for (var i = 0; i < data.length; i++) {
+    var container = document.getElementById('container');
+    //console.log(data[i]._id);
+
+    let box = document.createElement('div');
+    box.setAttribute("class", "box");
+
+    let input = document.createElement('input');
+    input.setAttribute("class", "toggle");
+    input.setAttribute("type", "checkbox");
+    input.setAttribute("id", data[i]._id);
+    box.appendChild(input);
+
+    let label = document.createElement('label');
+    label.setAttribute("class", "labelHeader");
+    label.setAttribute("for", data[i]._id);
+    box.appendChild(label);
+
+    let placeNameHeader = document.createElement('h1');
+    placeNameHeader.textContent = data[i].name;
+    placeNameHeader.setAttribute("class", "placeNameHeader");
+    label.appendChild(placeNameHeader);
+
+    let boxBody = document.createElement('div');
+    boxBody.setAttribute("class", "boxBody");
+    box.appendChild(boxBody);
+
+    let img = document.createElement('img');
+    img.setAttribute("src", "image.jpg");
+    img.setAttribute("class", "satImage");
+    boxBody.appendChild(img);
+
+    let desc = document.createElement('p');
+    desc.textContent = data[i].name + " is a/an " + data[i].description + ".";
+    desc.setAttribute("class", "desc");
+    boxBody.appendChild(desc);
+
+    container.appendChild(box);
+  }
+
+
 }
