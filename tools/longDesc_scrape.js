eb98e926758d6aa33b19a0454c949a01d28a8aeb
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-20 14:27:52 -0400

    nlp tinkering before cleanup

--- tools/longDesc_scrape.js
+++ tools/longDesc_scrape.js
@@ -6,7 +6,7 @@ var nlp = require('compromise');
 gather();
 
 function gather(){
-  let url = 'https://en.wikipedia.org/wiki/Wild_Horse_Wind_Farm';
+  let url = 'https://en.wikipedia.org/wiki/Greensville_Correctional_Center';
 
   request(url, function(err, res, body){
     console.log('Requesting');
@@ -19,25 +19,35 @@ function gather(){
         let textList = wholeText.split(/\n/);
         let wordList = [];
 
-        console.log(nlp('This is a cat.').sentences());
-
-        console.log(nlp('at').sentences());
+        // let word = nlp('swim');
+        // word.verbs().conjugation();
+        // console.log(word.verbs().conjugate());
+        //
+        // let doc = nlp('a bottle of beer on the wall.')
+        // doc.nouns(0).toPlural()
+        // console.log(doc.out('text'));
+        //
+        // console.log(nlp('32°47′30″N 108°04′02″W﻿ / ﻿32.7917425°N 108.0672635°W﻿ / 32.7917425; -108.0672635Coordinates: 32°47′30″N 108°04′02″W﻿ / ﻿32.7917425°N 108.0672635°W﻿ / 32.7917425; -108.0672635').sentences().length);
 
         for (var i = 0; i < textList.length; i++) {
-          //console.log(textList[i]);
-          //nlp(textList[i]).sentences;
-          //console.log('After');
-
-          if (textList[i] == ''){
-
-          } else if (textList[i].includes('\t'))  {
-
-          } else {
+          // console.log(textList[i]);
+          // console.log(nlp(textList[i]).sentences().length);
+          //let length = ;
+          if(nlp(textList[i]).sentences().length > 1){
             wordList.push(textList[i])
           }
 
+
+          // if (textList[i] == ''){
+          //
+          // } else if (textList[i].includes('\t'))  {
+          //
+          // } else {
+          //   wordList.push(textList[i])
+          // }
+
         }
-        //console.log(wordList);
+        console.log(wordList);
     }
   })
 }
