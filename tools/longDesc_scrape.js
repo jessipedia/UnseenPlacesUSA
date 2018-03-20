fc9bfc4ef97fb6e749f71f1f55e4b4a3dbb6f3dc
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-20 16:51:57 -0400

    Built better if satements for scraping

--- tools/longDesc_scrape.js
+++ tools/longDesc_scrape.js
@@ -5,7 +5,7 @@ var nlp = require('compromise');
 
 var data;
 
-var readableStream = fs.createReadStream('./lists/florida_state_prisons.txt');
+var readableStream = fs.createReadStream('./lists/alaska_state_prisons.txt');
 
 readableStream.setEncoding('utf8');
 
@@ -15,47 +15,60 @@ readableStream.on('data', function(chunk) {
 
 readableStream.on('end', function() {
    let list = data.split(',');
-   //console.log(list);
-   let listFix = [];
 
-  for (var i = 0; i < list.length; i++) {
-      //console.log(list[i]);
+  for (let i = 0; i < list.length; i++) {
       counter = i;
-      var name = list[i].trim();
+      let name = list[i].trim();
       name = name.replace(/undefined/, '');
-      var strp = list[i].trim();
-      strp = list[i].replace(/\([^)]*\)/, '');
+      let strp = name.replace(/\([^)]*\)/, '');
       strp = encodeURIComponent(strp);
-      //console.log(strp);
-      //console.log(name);
-      listFix.push(name);
 
+      gather(strp, name);
   }
-  console.log(listFix);
 });
 
-gather();
+
 
 function gather(place, nm){
-  let url = 'https://en.wikipedia.org/wiki/John_M._Sully';
+  let wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+ place;
 
-  request(url, function(err, res, body){
-    console.log('Requesting');
+  //console.log(wikiapi);
+
+  request(wikiapi, function(err, res, body) {
     if (err != null) {
-        console.log("Request " + err);
+        //console.log("Request " + err);
         return
     } else {
-        var $ = cheerio.load(body);
-        let textList = $.text().split(/\n/);
-        let wordList = [];
+        var parsed = JSON.parse(body);
 
-        for (var i = 0; i < textList.length; i++) {
-          if(nlp(textList[i]).sentences().length > 1){
-            wordList.push(textList[i])
-          }
+        if (parsed[3][0]) {
+          var url = parsed[3][0];
+        } else {
+          console.log(parsed[0]);
+          return
         }
-        let para = wordList[0].replace(/\[\d\]/g, '')
-        console.log(para);
-    }
+      }
   })
+
+  //let url = 'https://en.wikipedia.org/wiki/John_M._Sully';
+
+  // request(url, function(err, res, body){
+  //   console.log('Requesting');
+  //   if (err != null) {
+  //       console.log("Request " + err);
+  //       return
+  //   } else {
+  //       var $ = cheerio.load(body);
+  //       let textList = $.text().split(/\n/);
+  //       let wordList = [];
+  //
+  //       for (var i = 0; i < textList.length; i++) {
+  //         if(nlp(textList[i]).sentences().length > 1){
+  //           wordList.push(textList[i])
+  //         }
+  //       }
+  //       let para = wordList[0].replace(/\[\d\]/g, '')
+  //       console.log(para);
+  //   }
+  //})
 }
