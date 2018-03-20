e94335b86e2e83c03111efea094574846be8be74
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-20 14:28:38 -0400

    Code cleaned up

--- tools/longDesc_scrape.js
+++ tools/longDesc_scrape.js
@@ -19,33 +19,10 @@ function gather(){
         let textList = wholeText.split(/\n/);
         let wordList = [];
 
-        // let word = nlp('swim');
-        // word.verbs().conjugation();
-        // console.log(word.verbs().conjugate());
-        //
-        // let doc = nlp('a bottle of beer on the wall.')
-        // doc.nouns(0).toPlural()
-        // console.log(doc.out('text'));
-        //
-        // console.log(nlp('32°47′30″N 108°04′02″W﻿ / ﻿32.7917425°N 108.0672635°W﻿ / 32.7917425; -108.0672635Coordinates: 32°47′30″N 108°04′02″W﻿ / ﻿32.7917425°N 108.0672635°W﻿ / 32.7917425; -108.0672635').sentences().length);
-
         for (var i = 0; i < textList.length; i++) {
-          // console.log(textList[i]);
-          // console.log(nlp(textList[i]).sentences().length);
-          //let length = ;
           if(nlp(textList[i]).sentences().length > 1){
             wordList.push(textList[i])
           }
-
-
-          // if (textList[i] == ''){
-          //
-          // } else if (textList[i].includes('\t'))  {
-          //
-          // } else {
-          //   wordList.push(textList[i])
-          // }
-
         }
         console.log(wordList);
     }
