a6b220ff889cf55bdc7770b9bd098fb10b23e7d8
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-03-26 15:51:52 -0400

    Now lightly refactored

--- tools/longDesc_scrape.js
+++ tools/longDesc_scrape.js
@@ -1,7 +1,6 @@
 var cheerio = require('cheerio');
 var fs = require('fs');
 var request = require('request');
-var nlp = require('compromise');
 var config = require('./config.js');
 var bottleneck = require('bottleneck');
 
@@ -35,7 +34,7 @@ readableStream.on('end', function() {
 
       limiter.schedule(() => gather(strp))
         .then(result => scrape(result))
-        .then(result => stusup(result));
+        .then(result => createObj(result));
   }
 });
 
@@ -43,7 +42,6 @@ readableStream.on('end', function() {
 
 function gather(place){
   return new Promise(resolve => {
-  //console.log(place);
   let wikiapi = 'https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search='+ place;
   let options = {
     uri: wikiapi,
@@ -70,65 +68,63 @@ function scrape(parsed){
     } else{
 
       if (parsed[3][0]){
-
-      let url = parsed[3][0];
-      //console.log(url);
-      //let nm = parsed[0];
-
-      let options = {
-        uri: url,
-        family: 4
-      }
-
-      request(options, function(err, res, body){
-        console.log('Scrape Request Made');
-
-        if (err != null) {
-            console.log("Scrape Request " + err + '\n' + '\t' + url);
-            resolve(null)
-        } else {
-          resolve(body);
+        let url = parsed[3][0];
+        let options = {
+          uri: url,
+          family: 4
         }
-      })
+
+        request(options, function(err, res, body){
+          console.log('Scrape Request Made');
+
+          if (err != null) {
+              console.log("Scrape Request " + err + '\n' + '\t' + url);
+              resolve(null)
+          } else {
+            resolve(body);
+          }
+        })
     } else {
-      console.log(parsed[0]);
-      resolve();
+      resolve(parsed[0]);
     }
 
       }
 })
 }
 
-function stusup(body){
-  //console.log(body);
+function createObj(body){
   return new Promise(resolve => {
 
-  let text = longDesc(body);
-  let latlon = geo(body);
+   if (body.includes('<!DOCTYPE html>')){
 
-  if (text && latlon){
-    latlon = latlon.split(';');
-    let lat = latlon[0];
-    let lon = latlon[1];
-    let mapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + '&key=' + config.staticKey;
+      let text = longDesc(body);
+      let latlon = geo(body);
 
-    request(mapsUrl, function(err, res, body){
-      console.log('Geocode Request Made');
-      if (err != null) {
-        console.log("Geocode Request " + err + '\n' + '\t' + mapsUrl);
-        return;
+      if (text && latlon){
+        latlon = latlon.split(';');
+        let lat = latlon[0];
+        let lon = latlon[1];
+        let mapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json?latlng=' + lat + ',' + lon + '&key=' + config.staticKey;
+
+        request(mapsUrl, function(err, res, body){
+          console.log('Geocode Request Made');
+          if (err != null) {
+            console.log("Geocode Request " + err + '\n' + '\t' + mapsUrl);
+            return;
+          } else{
+            let parsed = JSON.parse(body);
+            console.log(parsed);
+            /////Return complete here
+          }
+        })
       } else{
-        let parsed = JSON.parse(body);
-        console.log(parsed);
+      ///// Return partially incomplete here
+      resolve();
       }
-
-    })
-
-    //console.log('\n' + 'Complete ' + '\n' + nm + '\n' + now + '\n' + latlon + '\n' + shortDesc + '\n' + url + '\n' + text);
     } else{
-    //console.log('\n' + 'Incomplete ' + '\n' + nm + '\n' + now + '\n' + latlon + '\n' + shortDesc  + '\n' + url + '\n'  + text);
+      ////Return Totally incomplete here too
+      resolve();
     }
-
   })
 }
 
@@ -136,11 +132,9 @@ function geo(body, url){
   let $ = cheerio.load(body);
   let coord = $(".geo").html();
   if (coord){
-    //console.log(url);
     coord = coord.toString();
     return(coord);
   } else{
-    //lon is the negative number in US (geo[1])
     return(null);
   }
 }
@@ -152,6 +146,5 @@ function longDesc(body){
   let text = para.text();
   text = text.replace(/\[citation needed\]/g, '');
   text = text.replace(/\[\d\]/g, '');
-
   return(text);
 }
